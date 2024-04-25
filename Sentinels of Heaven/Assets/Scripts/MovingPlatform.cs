f774a72aee066d67dc5222d16eb9030bd0dc8e0a
Bosspug <eugeniojlopezt@gmail.com>
2024-04-24 23:55:22 -0400

    Movingplatforms Test (LOOK BELOW COMMIT)
    DONT LOOK AT THIS LOOK AT "bricked up tilemap"

---
+++ Sentinels of Heaven/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public float speed; //speed of plat
+    public int startingPoint; //starting index (plat pos)
+    public Transform[] points; //array of points
+
+    private int i; //index of array
+    
+    void Start()
+    {
+        transform.position = points[startingPoint].position;   
+    }
+
+   
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (collision.transform.position.y - 0.8f > transform.position.y)
+            {
+                collision.transform.SetParent(transform);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
